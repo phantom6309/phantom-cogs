5ea1cb2aa1dbf310ace6ff1d66661adf90e38fa8
phantom6309 <mustafabagiskondu@gmail.com>
2022-12-14 20:34:22 +0300

    yjy
    th

---
+++ text/hikaye.py
@@ -0,0 +1,39 @@
+# Post animal pics by Eslyium#1949 & Yukirin#0048
+
+# Discord
+import discord
+import requests
+# Red
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import box, humanize_list
+from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
+import random
+# Libs
+import aiohttp
+
+BaseCog = getattr(commands, "Cog", object)
+class Hikaye(BaseCog):
+    """Animal commands."""
+
+    def __init__(self, bot):
+        self.bot = bot
+  
+
+    @commands.command()
+    @commands.cooldown(1, 60, commands.BucketType.guild)
+    async def add_text(ctx, *, text):
+     texts.append(text)
+    await ctx.send(f"Text added: {text}")
+ 
+# Define a command to get a random text from the list
+@bot.command()
+async def random_text(ctx):
+  text = random.choice(texts)
+  await ctx.send(f"Random text: {text}")
+
+
+    
+def cog_unload(self):
+self.bot.loop.create_task(self.session.close())
+
+__del__ = cog_unload
