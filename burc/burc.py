7e735da2bcf36cbe913edcd68dc6039ce79b8a84
phantom6309 <mustafabagiskondu@gmail.com>
2022-12-16 22:26:30 +0300

    up

--- burc/burc.py
+++ burc/burc.py
@@ -254,7 +254,7 @@ class Burc(BaseCog):
            url = "https://api.collectapi.com/health/dutyPharmacy"
            headers = {'authorization':'apikey 30d5SeFaSenqEvTFHaJjXI:71pwcsZRZA2qxRi2vNJVmX','content-type':'application/json',}
            params = {'ilce': ilce,'il': il,}
-           r = response = requests.get(url, headers=headers, params=params)
+           r = requests.get(url, headers=headers, params=params)
            veri = r.json()
            isim = veri['name']
            adres = veri['address']
