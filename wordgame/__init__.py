dde3f26679e9e24c4fae3868b3876fad1cd87a99
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2022-12-29 16:27:01 +0300

    Update __init__.py

--- wordgame/__init__.py
+++ wordgame/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from .wordgame import Wordgame
 import json
+
 async def setup(bot):
     bot.add_cog(Wordgame(bot))
 
-with open('wordlist.json', 'r') as f:
-      word_list = json.load(f)
 
