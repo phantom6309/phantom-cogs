85614e6fc4537effba9d40130854af8f57b31c08
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-03 19:50:38 +0300

    up

--- kelime/kelime.py
+++ kelime/kelime.py
@@ -41,7 +41,7 @@ class Kelime(commands.Cog):
         self.scores[user.id] += len(word)
         emoji = '\N{THUMBS UP SIGN}'
         await message.add_reaction(emoji)
-     else:
+     if word in self.used_words:
         self.scores[user.id] -= len(word)
         await self.game_channel.send(f"{word} kelimesi zaten kullanılmış.Yedin eksiyi.")
         emoji2 = '\N{THUMBS DOWN SIGN}'
