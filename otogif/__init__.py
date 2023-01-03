3b9a4ea29382c5fa11b5e7b66af7c54a6dd5fbe3
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-03 21:48:48 +0300

    htgh

--- otogif/__init__.py
+++ otogif/__init__.py
@@ -2,7 +2,7 @@ from .otogif import Otogif
 import sqlite3
 
 def setup(bot):
-    bot.add_cog(Otogif(bot))
     conn = sqlite3.connect('gif_responder.db')
     bot = Otogif(bot, conn)
     bot.init_db()
+    bot.add_cog(Otogif(bot)) 
