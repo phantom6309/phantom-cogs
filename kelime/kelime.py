3d1adf9ecc7ba9c3ad3d4ef3c633d502a035033b
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-01-03 15:41:08 +0300

    Update kelime.py

--- kelime/kelime.py
+++ kelime/kelime.py
@@ -109,8 +109,7 @@ class Kelime(commands.Cog):
      if message.author == self.bot.user:
         return 
      if message.channel == self.game_channel and not message.content.startswith("."):
-           word = message.content.strip()
-            word = word.lower()
+            word = message.content.strip.lower()
             if word[0] == self.current_word[-1] and word in self.word_list:
                 self.current_word = word
                 await self.give_points(message.author, word)
