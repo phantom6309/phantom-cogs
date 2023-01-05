b8a0f587a6dbe26391638ba0057169cac40b96d4
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-05 23:00:02 +0300

    UP

--- crabrave/crabrave.py
+++ crabrave/crabrave.py
@@ -17,7 +17,7 @@ CRAB_LINK = "https://youtu.be/gDLE3LikgUs"
 
 MIKU_LINK = "https://youtu.be/qeJjQGF6gz4"
 
-WEDNES_LINK = "https://youtu.be/NakTu_VZxJ0"
+WEDNES_LINK = "https://youtu.be/e0fFaUQdHz0"
 
 FONT_FILE = "https://github.com/matomo-org/travis-scripts/raw/master/fonts/Verdana.ttf"
 log = logging.getLogger("red.trusty-cogs.crabrave")
