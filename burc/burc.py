f02aeff4be02be2c6bb84f29c7e603cb6d14a345
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-06 18:16:07 +0300

    up

--- burc/burc.py
+++ burc/burc.py
@@ -290,7 +290,7 @@ class Burc(BaseCog):
            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
            response = requests.get(URL, headers=headers)
            data = response.json()
-           anlam = data['anlam']
+           anlam = data['anlamlarListe'][0]['anlam']
            await ctx.send(f'{anlam}')
 
     def cog_unload(self):
