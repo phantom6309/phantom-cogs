d17ed12e6796057975ca017e05a88e4970db81ee
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-01-06 17:08:38 +0300

    Update burc.py

--- burc/burc.py
+++ burc/burc.py
@@ -287,7 +287,8 @@ class Burc(BaseCog):
     @commands.command()
     async def tdk(ctx, word: str):
            URL = f"https://sozluk.gov.tr/gts?ara={word}"
-           response = requests.get(URL)
+           headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+           response = requests.get(URL, headers=headers)
            data = response.json()
 
            word = data[0]['madde']
