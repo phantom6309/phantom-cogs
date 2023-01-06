62047ee219cbecb9d0397d8c99fe6ce37c615ce0
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-06 18:43:40 +0300

    up

--- burc/burc.py
+++ burc/burc.py
@@ -288,7 +288,8 @@ class Burc(BaseCog):
     async def tdk(self, ctx, word:str):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
         URL = f"https://sozluk.gov.tr/gts?ara={word}"
-        response = requests.get(URL, headers=headers)
+        r = requests.get(URL, headers=headers)
+        response = r.json()
         embed = discord.Embed(title="Kur")
 
         # Add the values from the response to the embed
