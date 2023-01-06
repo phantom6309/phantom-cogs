8b32af2ed6d51bdbba8319cb68720b07c0038ad9
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-06 19:02:30 +0300

    jlpkj

--- burc/burc.py
+++ burc/burc.py
@@ -290,7 +290,7 @@ class Burc(BaseCog):
     async def tdk(self, ctx, word):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
         url = f"https://sozluk.gov.tr/gts?ara={word}"
-        response = requests.get(url, header=headers)
+        response = requests.get(url, headers=headers)
         data = response.json()
 
         definition = data[0]["anlamlarListe"][0]["anlam"]
