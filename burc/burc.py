9cdc0cf174169688354fca3745997bc129ce7144
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-06 18:38:58 +0300

    up

--- burc/burc.py
+++ burc/burc.py
@@ -285,7 +285,7 @@ class Burc(BaseCog):
 
 
     @commands.command()
-    async def kur(self, ctx, word:str):
+    async def tdk(self, ctx, word:str):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
         URL = f"https://sozluk.gov.tr/gts?ara={word}"
         response = requests.get(URL, headers=headers)
