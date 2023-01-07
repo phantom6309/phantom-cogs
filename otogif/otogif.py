26fe862944d13dd5cde9b46f36d0acee5edabbab
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-07 22:38:38 +0300

    up

--- otogif/otogif.py
+++ otogif/otogif.py
@@ -1,68 +1,50 @@
-# -*- coding: utf-8 -*-
 import discord
-from redbot.core import commands
-import sqlite3
+from redbot.core import commands, Config
 import random
 
 class Otogif(commands.Cog):
-    def __init__(self, bot, db_conn):      
+    def __init__(self, bot):      
         self.bot = bot
-        self.conn = db_conn       
-    def init_db(self):
-
-        c = self.conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS trigger_gif_pairs
-                     (trigger text, gif_url text)''')
-        self.conn.commit()
+        self.config = Config.get_conf(self, identifier=1234567890)
+        self.config.register_guild(gifs=[])
 
     @commands.Cog.listener()
     async def on_message(self, message):
       if message.author == self.bot.user:
          return
 
-      c = self.conn.cursor()
-      c.execute('''SELECT gif_url FROM trigger_gif_pairs WHERE trigger=?''',
-              (message.content,))
-      gif_urls = c.fetchall()
-      if gif_urls:
-        gif_url = random.choice(gif_urls)[0]  # Select a random gif URL from the list
+      gifs = await self.config.guild(message.guild).gifs()
+      matching_gifs = [gif for gif in gifs if gif["trigger"] == message.content]
+      if matching_gifs:
+        gif = random.choice(matching_gifs)
         embed = discord.Embed()
-        embed.set_image(url=gif_url)
+        embed.set_image(url=gif["gif_url"])
         await message.channel.send(embed=embed)
-
 
     @commands.command()
     @commands.has_permissions(manage_guild=True)
     async def addgif(self, ctx, gif_url: str, trigger: str):
-     c = self.conn.cursor()
-     c.execute('''INSERT INTO trigger_gif_pairs (trigger, gif_url)
-                 VALUES (?, ?)''', (trigger, gif_url))
-     self.conn.commit()
+     gifs = await self.config.guild(ctx.guild).gifs()
+     gifs.append({"trigger": trigger, "gif_url": gif_url})
+     await self.config.guild(ctx.guild).gifs.set(gifs)
      await ctx.send(f'Added trigger "{trigger}" with gif URL "{gif_url}"')
-
 
     @commands.command()
     @commands.has_permissions(manage_guild=True)
     async def listgifs(self, ctx):
-        c = self.conn.cursor()
-        c.execute('''SELECT trigger FROM trigger_gif_pairs''')
-        triggers = c.fetchall()
-        if not triggers:
+        gifs = await self.config.guild(ctx.guild).gifs()
+        if not gifs:
            await ctx.send('No triggers found')
         else:
            message = 'Triggers:\n'
-           for trigger, in triggers:
-               message += f'- {trigger}\n'
+           for gif in gifs:
+               message += f'- {gif["trigger"]}\n'
            await ctx.send(message)
 
     @commands.command()
     @commands.has_permissions(manage_guild=True)
     async def removegif(self, ctx, *triggers: str):
-        c = self.conn.cursor()
-        for trigger in triggers:
-            c.execute('''DELETE FROM trigger_gif_pairs WHERE trigger=?''', (trigger,))
-        self.conn.commit()
+        gifs = await self.config.guild(ctx.guild).gifs()
+        gifs = [gif for gif in gifs if gif["trigger"] not in triggers]
+        await self.config.guild(ctx.guild).gifs.set(gifs)
         trigger_str = ', '.join(triggers)
-        await ctx.send(f'Removed triggers "{trigger_str}"')
-
-
