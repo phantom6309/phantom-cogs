364d8290c7ddf2a280b91c64a6ed6b45122df119
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-07 21:32:52 +0300

    up

--- gununsorusu/gununsorusu.py
+++ gununsorusu/gununsorusu.py
@@ -11,55 +11,57 @@ from random import choice
 
 
 class Gununsorusu(commands.Cog):
-    """günün sorusu"""
     def __init__(self, bot):
         self.bot = bot
-        self.items = []
-        self.filename = 'items.json'
-        # Load items from file if it exists
-        try:
-            with open(self.filename, 'r') as f:
-                self.items = json.load(f)
-        except FileNotFoundError:
-            pass
-
+        self.config = Config.get_conf(self, identifier=965854745)
+        self.config.register_guild(items=[])
+    
     @commands.command()
     async def soruekle(self, ctx, *, item):
         """Havuza soru ekleyin"""
-        self.items.append(item)
-        with open(self.filename, 'w') as f:
-            json.dump(self.items, f)
+        guild_id = ctx.guild.id
+        items = await self.config.guild(ctx.guild).items()
+        items.append(item)
+        await self.config.guild(ctx.guild).items.set(items)
         await ctx.send(f'Soru eklendi!')
-
+    
     @commands.command()
     async def gününsorusu(self, ctx):
         """Günün sorusunu isteyin"""
-        if not self.items:
+        guild_id = ctx.guild.id
+        items = await self.config.guild(ctx.guild).items()
+        if not items:
             await ctx.send('No items have been saved yet!')
         else:
-            item = choice(self.items)
+            item = choice(items)
             await ctx.send(f'Günün sorusu: "{item}"')
-            self.items.remove(item)
+            items.remove(item)
+            await self.config.guild(ctx.guild).items.set(items)
+    
     @commands.command()
     async def sorulistesi(self, ctx):
         """Soru havuzunu görüntüleyin"""
-        if not self.items:
+        guild_id = ctx.guild.id
+        items = await self.config.guild(ctx.guild).items()
+        if not items:
             await ctx.send('Havuzda soru yok!')
         else:
-            item_list = '\n'.join(self.items)
+            item_list = '\n'.join(items)
             await ctx.send(f'Havuzdaki bütün sorular:\n{item_list}')
 
     @commands.command()
     async def çıkart(self, ctx, *, item):
-        """Listeden soru çıkartın"""
-        try:
-            self.items.remove(item)
-        except ValueError:
-            await ctx.send(f'Item "{item}" sorusu listede bulunamadı!')
-        else:
-            with open(self.filename, 'w') as f:
-                json.dump(self.items, f)
-            await ctx.send(f'Removed "{item}" sorusu listeden çıkarıldı!')
+         """Listeden soru çıkartın"""
+         guild_id = ctx.guild.id
+         items = await self.config.guild(ctx.guild).items()
+         try:
+            items.remove(item)
+         except ValueError:
+          await ctx.send(f'Item "{item}" sorusu listede bulunamadı!')
+         else:
+           await self.config.guild(ctx.guild).items.set(items)
+           await ctx.send(f'Removed "{item}" sorusu listeden çıkarıldı!')
+         
     
     @commands.command()
     async def temizle(self, ctx):
@@ -70,3 +72,10 @@ class Gununsorusu(commands.Cog):
         await ctx.send('soru havuzu temizlendi!')
   
     
+    @commands.command()
+    async def temizle(self, ctx):
+     """Tüm listeyi temizleyin"""
+     await self.config.guild(ctx.guild).items.set([])
+     await ctx.send('soru havuzu temizlendi!')
+     
+
