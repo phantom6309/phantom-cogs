372b991e88c139532dd42c321c639f6016011e10
phantom6309 <mustafabagiskondu@gmail.com>
2023-01-23 14:45:42 +0300

    new cog

---
+++ çeviri/__init__.py
@@ -0,0 +1,7 @@
+from .çeviri import Çeviri
+
+def setup(bot):
+    bot.add_cog(Çeviri(bot))
+    bot = Otogif(bot)
+    
+    
---
+++ çeviri/çeviri.py
@@ -0,0 +1,23 @@
+import discord
+from redbot.core import commands
+import googletrans
+
+class Çeviri(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.translator = googletrans.Translator()
+
+    @commands.command()
+    async def setchannel(self, ctx, channel: discord.TextChannel):
+        self.channel_id = channel.id
+        await ctx.send(f"Translate channel set to {channel.mention}")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id == self.channel_id:
+            src = self.translator.detect(message.content).lang
+            dest = 'tr' if src == 'en' else 'en'
+            translation = self.translator.translate(message.content, dest=dest)
+            await message.channel.send(translation.text)
+
+
