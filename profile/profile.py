2854420007b85a480225ca0c345ff333e6dfc957
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-03-21 20:30:16 +0300

    Update profile.py

--- profile/profile.py
+++ profile/profile.py
@@ -67,7 +67,7 @@ class Profile(commands.Cog):
         
     @profil.command(name="değiştir")
     async def _değiştir(self, ctx, field: str):
-       fields = {
+     fields = {
         "isim": "name",
         "yaş": "age",
         "cinsiyet": "gender",
@@ -77,48 +77,47 @@ class Profile(commands.Cog):
         "en sevdiğiniz film": "favorite_movies",
         "hobiler": "hobbies",
         "hakkımda": "about_me"
-      }
-     field = fields.get(field.lower())
-     if not field:
+    }
+    field = fields.get(field.lower())
+    if not field:
         return await ctx.send("Geçersiz alan adı!")
-     await ctx.author.send(f"Lütfen yeni {field} değerini girin.")
-     value = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
-     await self.config.member(ctx.author).set_raw(field, value=value.content)
-     await ctx.send(f"{field} değeri başarıyla güncellendi!")
-    
-    @profil.command(name="göster")
-    async def _göster(self, ctx, member: discord.Member = None):
-     if member is None:
-         member = ctx.author
+    await ctx.author.send(f"Lütfen yeni {field} değerini girin.")
+    value = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+    await self.config.member(ctx.author).set_raw(field, value=value.content)
+    await ctx.send(f"{field} değeri başarıyla güncellendi!")
 
-     name = await self.config.member(member).name()
-     age = await self.config.member(member).age()
-     gender = await self.config.member(member).gender()
-     city = await self.config.member(member).city()
-     occupation = await self.config.member(member).occupation()
-     favorite_tv_shows = await self.config.member(member).favorite_tv_shows()
-     favorite_movies = await self.config.member(member).favorite_movies()
-     hobbies = await self.config.member(member).hobbies()
-     about_me = await self.config.member(member).about_me()
+@profil.command(name="göster")
+async def _göster(self, ctx, member: discord.Member = None):
+    if member is None:
+        member = ctx.author
 
-     embed = discord.Embed(title=f"{member.display_name}'s Profile", color=0x00ff00)
-     embed.set_thumbnail(url=member.avatar_url)
-     fields = [
-         ("İsim", name or "Bilinmiyor", True),
-         ("Yaş", age or "Bilinmiyor", True),
-         ("Cinsiyet", gender or "Bilinmiyor", True),
-         ("Şehir", city or "Bilinmiyor", False),
-         ("Meslek", occupation or "Bilinmiyor", False),
-         ("En Sevdiğiniz TV Programları", favorite_tv_shows or "Bilinmiyor", False),
-         ("En Sevdiğiniz Filmler", favorite_movies or "Bilinmiyor", False),
-         ("Hobiler", hobbies or "Bilinmiyor", False),
-         ("Hakkımda", about_me or "Bilinmiyor", False),
-     ]
-     for name, value, inline in fields:
-         embed.add_field(name=name, value=value, inline=inline)
-     embed.set_image(url=member.avatar_url_as(size=1024))
-     await ctx.send(embed=embed)
+    name = await self.config.member(member).name()
+    age = await self.config.member(member).age()
+    gender = await self.config.member(member).gender()
+    city = await self.config.member(member).city()
+    occupation = await self.config.member(member).occupation()
+    favorite_tv_shows = await self.config.member(member).favorite_tv_shows()
+    favorite_movies = await self.config.member(member).favorite_movies()
+    hobbies = await self.config.member(member).hobbies()
+    about_me = await self.config.member(member).about_me()
 
+    embed = discord.Embed(title=f"{member.display_name}'s Profile", color=0x00ff00)
+    embed.set_thumbnail(url=member.avatar_url)
+    fields = [
+        ("İsim", name or "Bilinmiyor", True),
+        ("Yaş", age or "Bilinmiyor", True),
+        ("Cinsiyet", gender or "Bilinmiyor", True),
+        ("Şehir", city or "Bilinmiyor", False),
+        ("Meslek", occupation or "Bilinmiyor", False),
+        ("En Sevdiğiniz TV Programları", favorite_tv_shows or "Bilinmiyor", False),
+        ("En Sevdiğiniz Filmler", favorite_movies or "Bilinmiyor", False),
+        ("Hobiler", hobbies or "Bilinmiyor", False),
+        ("Hakkımda", about_me or "Bilinmiyor", False),
+    ]
+    for name, value, inline in fields:
+        embed.add_field(name=name, value=value, inline=inline)
+    embed.set_image(url=member.avatar_url_as(size=1024))
+    await ctx.send(embed=embed)
 
     
     
