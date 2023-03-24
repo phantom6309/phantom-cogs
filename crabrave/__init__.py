7204e8d99e9c2664794f7ca1ebde3f12657a3f49
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-03-24 10:15:02 +0300

    Update __init__.py

--- crabrave/__init__.py
+++ crabrave/__init__.py
@@ -7,6 +7,6 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot):
-    cog = CrabRave(bot)
-    bot.add_cog(cog)
+async def setup(bot):
+    cog = Crabrave(bot)
+    await bot.add_cog(cog)
