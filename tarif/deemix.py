997465b29c553d59b9eb8289f0b50cf83190aacc
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-20 00:37:23 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -9,8 +9,8 @@ class Deemix(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=7364528762)
-        self.config.register_guilds("email"=None, 
-         "password"=None
+        self.config.register_guild(email=None, 
+         password=None
          )
         
         
