d388c163b433b1bc03e6928e0a050f886fce61a2
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-24 09:07:40 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -2,8 +2,7 @@
 import discord
 from redbot.core import checks,commands,Config 
 from redbot.core.data_manager import cog_data_path
-import deezloader
-from deezloader import Login
+import pydeezloader
 
 class Deemix(commands.Cog):
     def __init__(self, bot):
@@ -17,25 +16,18 @@ class Deemix(commands.Cog):
     
     @commands.command()
     @checks.admin_or_permissions(manage_guild=True)
-    async def setbaz(self, ctx, mail,passw):
-     await self.config.guild(ctx.guild).email.set(mail)
-     await self.config.guild(ctx.guild).password.set(passw)
+    async def setbaz(self, ctx, arl):
+     await self.config.guild(ctx.guild).arl.set(arl)
      await ctx.send("profil ayarlandı!")
         
     @commands.command()
     async def download(self, ctx, url,quality):
-       email = await self.config.guild(ctx.guild).email()
-       password = await self.config.guild(ctx.guild).password() 
-       downloa = Login(
-	    email = my_deezer_email,
-	    password = my_deezer_password
-        )
-       downloa.download_trackspo(
-	    #YOUR SPOTIFY TRACK LINK,
-	    output_dir =str(cog_data_path(self) / url),
-	    quality_download = quality,
-	    recursive_quality = True,
-	    recursive_download = True,
-	    not_interface = True,
-	    method_save = 2
-        )
+       arl = await self.config.guild(ctx.guild).arl()
+       download = pydeezloader.Login(arl) 
+       download.download_trackspo(
+	"Insert the Spotify link of the track to download",
+	output = str(cog_data_path(self) /),
+	quality = "MP3_320",
+	recursive_quality = False,
+	recursive_download = False
+	not_interface = False
