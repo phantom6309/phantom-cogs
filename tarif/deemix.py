dea79c11b9866c4f4b96ef21866f0ab64e77dd40
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-24 16:19:23 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -30,6 +30,6 @@ class Deemix(commands.Cog):
 	recursive_quality = False,
 	recursive_download = False,
         not_interface = False,
-        method_save = 1
+        method_save = 2
         )
        
