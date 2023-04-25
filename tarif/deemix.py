56e51c4b1af5cb378fc578da5a842f228eeb0c5f
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-25 16:54:53 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -43,6 +43,7 @@ class Deemix(commands.Cog):
        song = downloa.download_trackspo(
         url,
 	output_dir = str(bundled_data_path(self)),
+        song_dir = output_dir,
 	quality_download = quality,
 	recursive_quality = False,
 	recursive_download = False,
