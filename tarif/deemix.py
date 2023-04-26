06e220a04c1c86178aae5a8dacef5d7c1d49025d
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 19:51:05 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -51,8 +51,8 @@ class Deemix(commands.Cog):
                file_path = os.path.join(folder, filename)
                try:
                   if os.path.isfile(file_path) or os.path.islink(file_path):
-                      os.unlink(file_path)
+                    os.unlink(file_path)
                   else os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
+                    shutil.rmtree(file_path)
                except Exception as e:
                    print('Failed to delete %s. Reason: %s' % (file_path, e))
