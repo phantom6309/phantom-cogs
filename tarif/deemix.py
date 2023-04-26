cbfff93d1372f7829701145617140f5115d3dbd4
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 19:08:18 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -54,4 +54,4 @@ class Deemix(commands.Cog):
                       os.unlink(file_path)
                   elif os.path.isdir(file_path):
                         shutil.rmtree(file_path)
-       await ctx.send("tamamlandı")
+                  await ctx.send("tamamlandı")
