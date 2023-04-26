5b414ce2b7a4d899f25b05fa9591373c4803fa21
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 21:57:53 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -47,7 +47,7 @@ class Deemix(commands.Cog):
                   download_link = send_to_transfersh(filepath, clipboard=False)
                   await ctx.send(download_link)
 
-         for root, dirs, files in os.walk(path):
+        
                   for f in files:
                     os.unlink(os.path.join(root, f))
                   for d in dirs:
