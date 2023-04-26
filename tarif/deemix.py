79d3d8a12bb6746a5ad70c4b8285a43bab40eb16
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 18:50:49 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -56,9 +56,9 @@ class Deemix(commands.Cog):
                         shutil.rmtree(file_path)
                         await ctx.send("tamamlandı")
 
-    @commands.command()
-    async def albüm(self, ctx, url, quality = None):
-       if quality == None:
+      @commands.command()
+      async def albüm(self, ctx, url, quality = None):
+        if quality == None:
           quality = "MP3_320"
        arl = await self.config.token()
        downloa = Login(arl) 
