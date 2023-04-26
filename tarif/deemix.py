8c262ec9a88491fa37f356407531e0c1636ddc3e
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 19:16:45 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -52,6 +52,6 @@ class Deemix(commands.Cog):
                try:
                   if os.path.isfile(file_path) or os.path.islink(file_path):
                       os.unlink(file_path)
-                  elif os.path.isdir(file_path):
+                  else os.path.isdir(file_path):
                         shutil.rmtree(file_path)
    
