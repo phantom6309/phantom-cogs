727db3b4c408934ee42cc0b38648c0b02ee2aa58
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 19:55:39 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -46,13 +46,7 @@ class Deemix(commands.Cog):
            with open(filepath, "rb") as f:
                download_link = send_to_transfersh(filepath, clipboard=False)
                await ctx.send(download_link)
-           folder = path
-           for filename in os.listdir(folder):
-               file_path = os.path.join(folder, filename)
-               try:
-                  if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                  elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-               except Exception as e:
-                   print('Failed to delete %s. Reason: %s' % (file_path, e))
+          for f in files:
+              os.unlink(os.path.join(root, f))
+          for d in dirs:
+              shutil.rmtree(os.path.join(root, d))
