82f1e09011aed9b6c7f7f472fb7cf24ddb23c392
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 22:24:17 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -47,8 +47,9 @@ class Deemix(commands.Cog):
                   download_link = send_to_transfersh(filepath, clipboard=False)
                   await ctx.send(download_link)
                   for filename in os.listdir(path):
-       filepath = os.path.join(dirpath, filename)
-       try:
-           shutil.rmtree(path)
-       except OSError:
-           os.remove(filepath)
+       with os.scandir(target_dir) as entries:
+          for entry in entries:
+              if entry.is_dir() and not entry.is_symlink():
+                 shutil.rmtree(entry.path)
+              else:
+                 os.remove(entry.path)
