04ed47aca797211df69112df78179a486d973586
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-26 12:43:43 +0300

    Update deemix.py

--- tarif/deemix.py
+++ tarif/deemix.py
@@ -43,8 +43,8 @@ class Deemix(commands.Cog):
           for filepath in glob.iglob(path + '/**/*', recursive=True):
               if os.path.isfile(filepath):
                  filename = os.path.basename(filepath)
-                 with open(file_path, "rb") as file:
-                      file_data = discord.File(file, filename=file_name.encode('raw_unicode_escape').decode('utf-8'))
+                 with open(file_path, "rb") as f:
+                      file_data = discord.File(f, filename=file_name.encode('raw_unicode_escape').decode('utf-8'))
                       await ctx.send(file=file_data)
                  os.remove(filepath)
        await ctx.send("tamamlandı")
