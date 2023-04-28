82d106f631f429ec4f66e4ba58c224a19f54b433
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-28 11:50:34 +0300

    Update astro.py

--- astro/astro.py
+++ astro/astro.py
@@ -30,7 +30,7 @@ class Astro(commands.Cog):
         soup = str(await self.get_burc_yorum(burc))
         soup2 = str(await self.get_burc_yorum2(burc))
         emotions = str(translator.translate(soup, dest="tr"))
-        love = str(translator.translate(soup, dest="tr"))
+        love = str(translator.translate(soup2, dest="tr"))
         # Use regular expressions to extract individual horoscope sections
         personal = re.search(r'Kişisel: (.+?)</p>', emotions).group(1)                        
         travel = re.search(r'Seyahat: (.+?)</p>', emotions).group(1)
