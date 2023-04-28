0cb0eeecf2953e6a29f12dffd646051fb1544643
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-28 11:47:55 +0300

    Update astro.py

--- astro/astro.py
+++ astro/astro.py
@@ -10,19 +10,27 @@ class Astro(commands.Cog):
         self.bot = bot
         
     async def get_burc_yorum(self, burc):
-        url = f"https://astrotalk.com/horoscope/todays-horoscope/{burc}"
-        page = requests.get(url).text
+        url = f"https://astrotalk.com/horoscope/todays-horoscope/{burc}
+        page = requests.get(url).text   
         soup = BeautifulSoup(page, "html.parser")
         burc_yorumu = soup.find("div", class_=f"parah_aries_horocope")
         return burc_yorumu
-        
+
+    async def get_burc_yorum2(self, burc):  
+        url2 = f"https://astrotalk.com/horoscope/todays-love-horoscope/{burc}"
+        page2 = requests.get(url2).text
+        soup2 = BeautifulSoup(page2, "html.parser")
+        burc_yorumu2 = soup2.find("div", class_=f"parah_aries_horocope")
+        return burc_yorumu2
+
     @commands.command()
     async def astro(self, ctx, burc:str):
         burc = burc.lower()
         translator = Translator()
         soup = str(await self.get_burc_yorum(burc))
+        soup2 = str(await self.get_burc_yorum2(burc))
         emotions = str(translator.translate(soup, dest="tr"))
-
+        love = str(translator.translate(soup, dest="tr"))
         # Use regular expressions to extract individual horoscope sections
         personal = re.search(r'Kişisel: (.+?)</p>', emotions).group(1)                        
         travel = re.search(r'Seyahat: (.+?)</p>', emotions).group(1)
@@ -39,5 +47,6 @@ class Astro(commands.Cog):
         embed.add_field(name="Kariyer", value=career, inline=False)
         embed.add_field(name="Sağlık", value=health, inline=False)
         embed.add_field(name="Duygular", value=feelings, inline=False)
+        embed.add_field(name="Aşk", value=love, inline=False)
 
         await ctx.send(embed=embed)
