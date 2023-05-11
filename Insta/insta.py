97f6e771092d9ed641dca843e42c3fa71ce666e0
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-05-11 18:38:10 +0300

    Update insta.py

--- Insta/insta.py
+++ Insta/insta.py
@@ -8,31 +8,22 @@ class Insta(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=7364528762)
-        self.config.register_member( login=None, 
-            password=None)
+        self.config.register_global(login=True, password=True
+        )
 
 
 
     @commands.command()
-    async def giriş(self, ctx):
-        await ctx.author.send("kullanıcı adınızı giriniz")
-        login = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
-        
-        await ctx.author.send("şifrenizi giriniz?")
-        password = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author)
-
-
-        await self.config.member(ctx.author).login.set(login.content)
-        await self.config.member(ctx.author).password.set(password.content)
-
-        
-        await ctx.send("Profiliniz başarıyla oluşturuldu!")
+    async def giriş(self, ctx, login:str, password:str):
+     await self.config.login.set(login)
+     await self.config.password.set(password)
+     await ctx.send("hesap ayarlandı!")
     
     @commands.command()
     async def insta(self, ctx, url:str):
         url = re.search(r'/p (.+?)/', url)
-        login = await self.config.member(member).login()
-        password = await self.config.member(member).password()
+        login  = await self.config.login()
+        password = await self.config.password()
         L = instaloader.Instaloader()
         L.login(login, password)
         post = Post.from_shortcode(L.context, url)
