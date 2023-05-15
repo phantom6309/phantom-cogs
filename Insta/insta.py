c765823e6411b8f86de150252125fef95f8482b4
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-05-16 00:56:02 +0300

    Update insta.py

--- Insta/insta.py
+++ Insta/insta.py
@@ -74,12 +74,7 @@ class Insta(commands.Cog):
                 with open(filepath, "rb") as f:
                   file = discord.File(str(filepath), filename)
                   await ctx.send(files=[file])
-        with os.scandir(path) as entries:
-            for entry in entries:
-                if entry.is_dir() and not entry.is_symlink():
-                   shutil.rmtree(entry.path)
-                else:
-                   os.remove(entry.path)
+
         
         
       
