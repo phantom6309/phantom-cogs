b9cc370aada4d30b7750e275ca7a511385074299
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-05-15 23:28:47 +0300

    Update insta.py

--- Insta/insta.py
+++ Insta/insta.py
@@ -5,12 +5,13 @@ from redbot.core.data_manager import bundled_data_path
 import instaloader
 import os, shutil
 import re
+import twtvt
 
 class Insta(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=7364528762)
-        self.config.register_global(login=True, password=True
+        self.config.register_global(login=True, password=True, twlogin=True, twpassword=True
         )
         
 
@@ -19,6 +20,12 @@ class Insta(commands.Cog):
     async def giriş(self, ctx, login:str, password:str):
      await self.config.login.set(login)
      await self.config.password.set(password)
+     await ctx.send("hesap ayarlandı!")
+ 
+    @commands.command()
+    async def giriştw(self, ctx, login:str, password:str):
+     await self.config.twlogin.set(twlogin)
+     await self.config.twpassword.set(twpassword)
      await ctx.send("hesap ayarlandı!")
     
     @commands.command()
@@ -32,6 +39,34 @@ class Insta(commands.Cog):
         L.download_video_thumbnails = False
         L.dirname_pattern = download_loc
         file=L.download_post(post, download_loc)
+        path = str(bundled_data_path(self))
+        for root, dirs, files in os.walk(path):
+         for filename in files:
+            ext = os.path.splitext(filename)[1]
+            if ext.lower() in [".mp4", "jpg", "jpeg"]:
+                filepath = os.path.join(root, filename)
+                with open(filepath, "rb") as f:
+                  file = discord.File(str(filepath), filename)
+                  await ctx.send(files=[file])
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_dir() and not entry.is_symlink():
+                   shutil.rmtree(entry.path)
+                else:
+                   os.remove(entry.path)
+                
+    @commands.command()
+    async def tw(self, ctx, url):
+        twlogin  = await self.config.twlogin()
+        twpassword = await self.config.twpassword()
+        twtvt.download_video(
+        target_links = url,
+        username= twlogin,
+        password= twpassword,
+        output= str(bundled_data_path(self)),
+        cookies_from_browser='brave',
+        until_link='https://twitter.com/twtvtOfficial/status/1599748329927499777',
+        )
         path = str(bundled_data_path(self))
         for root, dirs, files in os.walk(path):
          for filename in files:
