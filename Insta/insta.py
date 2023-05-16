d18dbf57305b7a5518863be0c78ae4921e8fc82e
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-05-16 23:54:48 +0300

    Update insta.py

--- Insta/insta.py
+++ Insta/insta.py
@@ -5,7 +5,6 @@ from redbot.core.data_manager import bundled_data_path
 import instaloader
 import os, shutil
 import re
-from twdown import TwdownAPI
 
 class Insta(commands.Cog):
     def __init__(self, bot):
@@ -52,36 +51,3 @@ class Insta(commands.Cog):
                         shutil.rmtree(entry.path)
                     else:
                         os.remove(entry.path)
-                        
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-     instagram_link_pattern = r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/[\w\/]+"                 
-     if re.search(instagram_link_pattern, content):
-        instagram_urls = re.findall(instagram_link_pattern, message.content)
-        for url in instagram_urls:
-            login  = await self.config.login()
-            password = await self.config.password()
-            L = instaloader.Instaloader()
-            L.login(login, password)
-            download_loc = str(bundled_data_path(self)) 
-            post = instaloader.Post.from_shortcode(L.context, url.split("/")[-2])
-            L.download_video_thumbnails = False
-            L.dirname_pattern = download_loc
-            file=L.download_post(post, download_loc)
-            path = str(bundled_data_path(self))
-            for root, dirs, files in os.walk(path):
-                for filename in files:
-                    ext = os.path.splitext(filename)[1]
-                    if ext.lower() in [".mp4", "jpg", "jpeg"]:
-                       filepath = os.path.join(root, filename)
-                       with open(filepath, "rb") as f:
-                            file = discord.File(str(filepath), filename)
-                            await ctx.send(files=[file])
-                with os.scandir(path) as entries:
-                     for entry in entries:
-                         if entry.is_dir() and not entry.is_symlink():
-                            shutil.rmtree(entry.path)
-                     else:
-                         os.remove(entry.path)
-        
-      
