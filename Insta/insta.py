b948d30fbedeb1462080f27e059ecc437437e69a
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-05-16 23:34:15 +0300

    Update insta.py

--- Insta/insta.py
+++ Insta/insta.py
@@ -11,7 +11,7 @@ class Insta(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=7364528762)
-        self.config.register_global(login=True, password=True, twlogin=True, twpassword=True
+        self.config.register_global(login=True, password=True
         )
         
 
@@ -22,11 +22,6 @@ class Insta(commands.Cog):
      await self.config.password.set(password)
      await ctx.send("hesap ayarlandı!")
  
-    @commands.command()
-    async def giriştw(self, ctx, twlogin:str, twpassword:str):
-     await self.config.twlogin.set(twlogin)
-     await self.config.twpassword.set(twpassword)
-     await ctx.send("hesap ayarlandı!")
     
     @commands.command()
     async def insta(self, ctx, url):
@@ -55,34 +50,11 @@ class Insta(commands.Cog):
                 else:
                    os.remove(entry.path)
                 
-    @commands.hybrid_command()
-    async def tw(self, ctx, url:str):
-        twdown = TwdownAPI(
-        sharelink = url,
-        dir_to_save =str(bundled_data_path(self))
-        )  
-        twdown.run()
-        path = str(bundled_data_path(self))
-        for root, dirs, files in os.walk(path):
-         for filename in files:
-            ext = os.path.splitext(filename)[1]
-            if ext.lower() in [".mp4", "jpg", "jpeg"]:
-                filepath = os.path.join(root, filename)
-                with open(filepath, "rb") as f:
-                  file = discord.File(str(filepath), filename)
-                  await ctx.send(files=[file])
-        with os.scandir(path) as entries:
-            for entry in entries:
-                if entry.is_dir() and not entry.is_symlink():
-                   shutil.rmtree(entry.path)
-                else:
-                   os.remove(entry.path)
-
-
-
+    
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
      twitter_link_pattern = r"(?:https?:\/\/)?(?:www\.)?twitter\.com\/[\w\/]+"
+     instagram_link_pattern = r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/[\w\/]+"
      if re.search(twitter_link_pattern, message.content):
         urls = re.findall(twitter_link_pattern, message.content)
         for url in urls:
@@ -106,7 +78,33 @@ class Insta(commands.Cog):
                         shutil.rmtree(entry.path)
                     else:
                         os.remove(entry.path)
-
- 
+                        
+     if re.search(instagram_link_pattern, content):
+        instagram_urls = re.findall(instagram_link_pattern, message.content)
+        for url in instagram_urls:
+            login  = await self.config.login()
+            password = await self.config.password()
+            L = instaloader.Instaloader()
+            L.login(login, password)
+            download_loc = str(bundled_data_path(self)) 
+            post = instaloader.Post.from_shortcode(L.context, url.split("/")[-2])
+            L.download_video_thumbnails = False
+            L.dirname_pattern = download_loc
+            file=L.download_post(post, download_loc)
+            path = str(bundled_data_path(self))
+            for root, dirs, files in os.walk(path):
+                for filename in files:
+                    ext = os.path.splitext(filename)[1]
+                    if ext.lower() in [".mp4", "jpg", "jpeg"]:
+                       filepath = os.path.join(root, filename)
+                       with open(filepath, "rb") as f:
+                            file = discord.File(str(filepath), filename)
+                            await ctx.send(files=[file])
+                with os.scandir(path) as entries:
+                     for entry in entries:
+                         if entry.is_dir() and not entry.is_symlink():
+                            shutil.rmtree(entry.path)
+                     else:
+                         os.remove(entry.path)
         
       
