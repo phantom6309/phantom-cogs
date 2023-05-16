481e700cd4505d8ea1825bd397f5cb3605b94b38
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-05-17 00:03:17 +0300

    Update insta.py

--- Insta/insta.py
+++ Insta/insta.py
@@ -27,27 +27,31 @@ class Insta(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-     twitter_link_pattern = r"(?:https?:\/\/)?(?:www\.)?twitter\.com\/[\w\/]+"
-     if re.search(twitter_link_pattern, message.content):
-        urls = re.findall(twitter_link_pattern, message.content)
-        for url in urls:
-            twdown = TwdownAPI(
-                sharelink=url,
-                dir_to_save=str(bundled_data_path(self))
-            )
-            twdown.run()
+     instagram_link_pattern = r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/[\w\/]+"
+     if re.search(instagram_link_pattern, content):
+        instagram_urls = re.findall(instagram_link_pattern, message.content)
+        for url in instagram_urls:
+            login  = await self.config.login()
+            password = await self.config.password()
+            L = instaloader.Instaloader()
+            L.login(login, password)
+            download_loc = str(bundled_data_path(self)) 
+            post = instaloader.Post.from_shortcode(L.context, url.split("/")[-2])
+            L.download_video_thumbnails = False
+            L.dirname_pattern = download_loc
+            file=L.download_post(post, download_loc)
             path = str(bundled_data_path(self))
             for root, dirs, files in os.walk(path):
                 for filename in files:
                     ext = os.path.splitext(filename)[1]
-                    if ext.lower() in [".mp4", ".jpg", ".jpeg"]:
-                        filepath = os.path.join(root, filename)
-                        with open(filepath, "rb") as f:
-                            file = discord.File(filepath, filename)
-                            await message.channel.send(files=[file])
-            with os.scandir(path) as entries:
-                for entry in entries:
-                    if entry.is_dir() and not entry.is_symlink():
-                        shutil.rmtree(entry.path)
-                    else:
-                        os.remove(entry.path)
+                    if ext.lower() in [".mp4", "jpg", "jpeg"]:
+                       filepath = os.path.join(root, filename)
+                       with open(filepath, "rb") as f:
+                            file = discord.File(str(filepath), filename)
+                            await ctx.send(files=[file])
+                with os.scandir(path) as entries:
+                     for entry in entries:
+                         if entry.is_dir() and not entry.is_symlink():
+                            shutil.rmtree(entry.path)
+                     else:
+                         os.remove(entry.path)
