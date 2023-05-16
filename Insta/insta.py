f5baa35a526e7bea7ff778ab89e3d1c17909dbc2
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-05-16 23:45:14 +0300

    Update insta.py

--- Insta/insta.py
+++ Insta/insta.py
@@ -1,5 +1,5 @@
 import discord
-from redbot.core import commands, app_commands
+from redbot.core import commands
 from redbot.core import Config
 from redbot.core.data_manager import bundled_data_path
 import instaloader
@@ -23,38 +23,12 @@ class Insta(commands.Cog):
      await ctx.send("hesap ayarlandı!")
  
     
-    @commands.command()
-    async def insta(self, ctx, url):
-        login  = await self.config.login()
-        password = await self.config.password()
-        L = instaloader.Instaloader()
-        L.login(login, password)
-        download_loc = str(bundled_data_path(self)) 
-        post = instaloader.Post.from_shortcode(L.context, url.split("/")[-2])
-        L.download_video_thumbnails = False
-        L.dirname_pattern = download_loc
-        file=L.download_post(post, download_loc)
-        path = str(bundled_data_path(self))
-        for root, dirs, files in os.walk(path):
-         for filename in files:
-            ext = os.path.splitext(filename)[1]
-            if ext.lower() in [".mp4", "jpg", "jpeg"]:
-                filepath = os.path.join(root, filename)
-                with open(filepath, "rb") as f:
-                  file = discord.File(str(filepath), filename)
-                  await ctx.send(files=[file])
-        with os.scandir(path) as entries:
-            for entry in entries:
-                if entry.is_dir() and not entry.is_symlink():
-                   shutil.rmtree(entry.path)
-                else:
-                   os.remove(entry.path)
+
                 
     
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
      twitter_link_pattern = r"(?:https?:\/\/)?(?:www\.)?twitter\.com\/[\w\/]+"
-     instagram_link_pattern = r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/[\w\/]+"
      if re.search(twitter_link_pattern, message.content):
         urls = re.findall(twitter_link_pattern, message.content)
         for url in urls:
@@ -79,7 +53,10 @@ class Insta(commands.Cog):
                     else:
                         os.remove(entry.path)
                         
-     elif re.search(instagram_link_pattern, content):
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+     instagram_link_pattern = r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/[\w\/]+"                 
+     if re.search(instagram_link_pattern, content):
         instagram_urls = re.findall(instagram_link_pattern, message.content)
         for url in instagram_urls:
             login  = await self.config.login()
