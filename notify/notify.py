820a169fea8aa5ca6c894033a78bcb0496bdd451
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2024-02-28 22:18:02 +0300

    Update notify.py

--- notify/notify.py
+++ notify/notify.py
@@ -1,7 +1,7 @@
 import discord
 from redbot.core import commands, Config
 
-class ReactionNotifier(commands.Cog):
+class Notify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)  # Change the identifier to a unique value
